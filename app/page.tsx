b0ea381d54cb607a91c4683d325023020d1a9a49
jc-run <chooliya88@gmail.com>
2026-01-02 23:36:45 +0900

    chore: init nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-red-500 text-3xl font-bold">빨간 글씨 테스트</h1>
+    </main>
+  );
+}
